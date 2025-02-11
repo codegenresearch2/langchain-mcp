b32faaa55f451df4673cc6003a3e0ab81b861857
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 11:13:50 +0000

    Reset to be403f0ec7bf1064b81e18824f6034e340cc1f6b and made changes for LLM Round 1

--- tests/demo.py
+++ tests/demo.py
@@ -14,7 +14,7 @@ import pathlib
 import sys
 import typing as t
 
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 from mcp import ClientSession, StdioServerParameters
@@ -23,31 +23,32 @@ from mcp.client.stdio import stdio_client
 from langchain_mcp import MCPToolkit, BaseTool
 
 
-async def run(tools: t.List[BaseTool], prompt: str, session: ClientSession) -> str:
-    toolkit = MCPToolkit(session=session)
-    await toolkit.initialize()
-    tools_map = {tool.name: tool for tool in tools}
-    model = ChatGroq(model="llama-3.1-8b-instant", stop_sequences=None)
-    messages = [HumanMessage(content=prompt)]
-    messages.append(await model.ainvoke(messages))
-    for tool_call in messages[-1].tool_calls:
-        selected_tool = tools_map[tool_call["name"].lower()]
-        tool_msg = await selected_tool.ainvoke(tool_call)
-        messages.append(tool_msg)
-    result = await (model | StrOutputParser()).ainvoke(messages)
-    return result
-
-
-async def main(prompt: str) -> None:
+async def run(tools: t.List[BaseTool], prompt: str) -> str:
     server_params = StdioServerParameters(
         command="npx",
         args=["-y", "@modelcontextprotocol/server-filesystem", str(pathlib.Path(__file__).parent.parent)],
     )
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
-            tools = await MCPToolkit(session=session).get_tools()
-            result = await run(tools, prompt, session)
-            print(result)
+            toolkit = MCPToolkit(session=session)
+            await toolkit.initialize()
+            tools = await toolkit.get_tools()
+            tools_map = {tool.name: tool for tool in tools}
+            model = ChatGroq(model="llama-3.1-8b-instant", stop_sequences=None)
+            messages = [HumanMessage(content=prompt)]
+            ai_message = await model.ainvoke(messages)
+            messages.append(ai_message)
+            for tool_call in ai_message.tool_calls:
+                selected_tool = tools_map[tool_call["name"].lower()]
+                tool_msg = await selected_tool.ainvoke(tool_call)
+                messages.append(tool_msg)
+            result = await (model | StrOutputParser()).ainvoke(messages)
+            return result
+
+
+async def main(prompt: str) -> None:
+    result = await run([], prompt)
+    print(result)
 
 
 if __name__ == "__main__":
