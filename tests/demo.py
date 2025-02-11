61bced2f0978aedc78964e57532d683172328ca8
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 16:11:25 +0000

    Reset to be403f0ec7bf1064b81e18824f6034e340cc1f6b and made changes for LLM Round 1

--- tests/demo.py
+++ tests/demo.py
@@ -1,30 +1,28 @@
 import asyncio
 import pathlib
 import sys
-import typing as t
 
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.tools.base import BaseTool
+from langchain_core.tools import BaseTool
 from langchain_groq import ChatGroq
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 from langchain_mcp import MCPToolkit
 
-async def run(tools: t.List[BaseTool], prompt: str) -> str:
+async def run(tools: list[BaseTool], prompt: str) -> str:
     model = ChatGroq(model="llama-3.1-8b-instant", stop_sequences=None)  # requires GROQ_API_KEY
     tools_model = model.bind_tools(tools)
     tools_map = {tool.name: tool for tool in tools}
-    messages = [HumanMessage(content=prompt)]
-    ai_message = t.cast(AIMessage, await tools_model.ainvoke(messages))
+    messages: list[BaseMessage] = [HumanMessage(prompt)]
+    ai_message = await tools_model.ainvoke(messages)
     messages.append(ai_message)
     for tool_call in ai_message.tool_calls:
         selected_tool = tools_map[tool_call["name"].lower()]
         tool_msg = await selected_tool.ainvoke(tool_call)
         messages.append(tool_msg)
-    response = await (tools_model | StrOutputParser()).ainvoke(messages)
-    return response
+    return await (tools_model | StrOutputParser()).ainvoke(messages)
 
 async def main(prompt: str) -> None:
     server_params = StdioServerParameters(
